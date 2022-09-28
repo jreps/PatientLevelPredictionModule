3b580936e633b2b6cc2a4801e85d594b1d546b6e
Jenna Reps <jreps@its.jnj.com>
2022-09-28 13:23:00 -0400

    updating to 0.0.7
    updating to 0.0.7

--- SettingsFunctions.R
+++ SettingsFunctions.R
@@ -8,7 +8,7 @@ createPatientLevelPredictionModuleSpecifications <- function(
   
   specifications <- list(
     module = "PatientLevelPredictionModule",
-    version = "0.0.6",
+    version = "0.0.7",
     remoteRepo = "github.com",
     remoteUsername = "jreps",
     settings = modelDesignList
